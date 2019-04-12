c0bc61546db02d01b3169db8ce800e82c5501b52
Andrew Bales <agbales@gmail.com>
2019-04-11 23:18:59 -0700

    overview font, cleanup

--- client/src/components/overview.js
+++ client/src/components/overview.js
@@ -26,69 +26,57 @@ export default class PushcartPrizeRankings extends React.Component {
     render() {
         let submittableOrange = { color: '#FC5B30' }
         let second = { color: '#00AF79', cursor: 'pointer'}
-        let secondNum = { fontFamily: "'Bungee Shade', cursive", margin: '5px 15px', fontSize: '4rem', color: '#00AF79'}
         let third = { color: '#FFC10E' }
         let link = { cursor: 'pointer'}
+        let threeRem = { fontSize: '3rem' }
+        let roboto = { fontFamily: 'roboto' }
 
         return(
-        <div>
-            <header className="App-header">
-                <div className="flex-container">
-                    <div className="overview-box">
-                        <GiAstronautHelmet className="bigIcon" style={submittableOrange} />
-                        <div>
-                            <h1 className="inline-block playfair" style={{fontSize: '6rem', marginRight: '10px'}}>
-                                { this.props.stats.totalSubmissions &&
-                                    this.props.stats.totalSubmissions.toString()
-                                }
+            <div>
+                <header className="App-header">
+                    <div className="flex-container">
+                        <div className="overview-box">
+                            <GiAstronautHelmet className="bigIcon" style={submittableOrange} />
+                            <h1 style={threeRem}>{ this.props.stats.totalSubmissions.toString() }</h1>
+                            <span style={roboto}>missions</span>
+                        </div>
+                        <div className="overview-box" >
+                            <GiLunarModule className="bigIcon" 
+                                        style={second} 
+                                        onClick={ ()=> this.toggleAcceptedVisible() } />
+                            <h1 onClick={ ()=> this.toggleAcceptedVisible() } 
+                                style={threeRem}>{ this.props.stats.totalAcceptances.toString() }
                             </h1>
-                            <h1 className="inline-block" style={{fontSize: '3rem', fontFamily: 'open sans'}}>  missions</h1>
-                            <span className="inline-block"> </span>
+                            <span style={roboto}>landings</span>
+                            {/* <span onClick={ ()=> this.toggleAcceptedVisible() } style={link}>landings</span> */}
+                        </div>
+                        <div className="overview-box">
+                            <GiBeamSatellite className="bigIcon" style={third} />
+                            <h1 style={threeRem}>{ this.props.stats.overallAcceptanceRate.toString() }%</h1>
+                            <span style={roboto}>success rate</span>
                         </div>
                     </div>
-                    <div className="overview-box" >
-                        <GiLunarModule className="bigIcon" 
-                                       style={second} 
-                                       onClick={ ()=> this.toggleAcceptedVisible() } />
-                        <div>
-                            <h1 onClick={ ()=> this.toggleAcceptedVisible() } style={secondNum} className="inline-block">
-                                { this.props.stats.totalAcceptances &&
-                                    this.props.stats.totalAcceptances.toString()
-                                } 
-                            </h1>
-                            <h2 style={{display: 'inline-block', marignRight: '10px'}}>  landings</h2>
-                        </div>
-                        {/* <span onClick={ ()=> this.toggleAcceptedVisible() } style={link}>landings</span> */}
+                    
+                    <div className="container">
+                        <AnnualStats annualStats={this.props.stats.years}/>
                     </div>
-                    <div className="overview-box">
-                        <GiBeamSatellite className="bigIcon" style={third} />
-                        <h1 style={{fontSize: '3rem'}}>{ this.props.stats.overallAcceptanceRate &&
-                                this.props.stats.overallAcceptanceRate.toString()
-                            }%
-                        </h1>
-                        <span style={{fontFamily: 'roboto'}}>success rate</span>
-                    </div>
-                </div>
-                
-                <div className="container">
-                    <AnnualStats annualStats={this.props.stats.years}/>
-                </div>
 
-                { this.state.showAcceptances 
-                    ? (<div> 
-                            <Button onClick={ ()=> this.toggleAcceptedVisible() }>Close</Button>
-                            <ul>
-                                { this.props.acceptances.map( (entry, i) => {
-                                    return(<li key={i}>
-                                                <span className="accepted"><FaCheckCircle className="green-checkmark" /></span>
-                                                <span className="list-title">{entry['Title']}</span>
-                                            </li>)
-                                })}
-                            </ul>
-                        </div>)
-                    : <div></div> 
-                }
-            </header>
-        </div>)
+                    { this.state.showAcceptances 
+                        ? (<div> 
+                                <Button onClick={ ()=> this.toggleAcceptedVisible() }>Close</Button>
+                                <ul>
+                                    { this.props.acceptances.map( (entry, i) => {
+                                        return(<li key={i}>
+                                                    <span className="accepted"><FaCheckCircle className="green-checkmark" /></span>
+                                                    <span className="list-title">{entry['Title']}</span>
+                                                </li>)
+                                    })}
+                                </ul>
+                            </div>)
+                        : <div></div> 
+                    }
+                </header>
+            </div>
+        )
     }
 }
